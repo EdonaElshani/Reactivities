a08fbe6e1c663540afe4b9931b534a2cdafb1a54
EdonaElshani <edonaelshani31@gmail.com>
2023-03-06 15:42:34 +0100

    activity dashboard

--- client-app/src/app/Layout/App.tsx
+++ client-app/src/app/Layout/App.tsx
@@ -3,26 +3,25 @@ import axios from 'axios';
 import { Header } from 'semantic-ui-react';
 import List from 'semantic-ui-react/dist/commonjs/elements/List';
 import { Activity } from '../models/activity';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
-      console.log('Data received..');
-    })
-  }, [])
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+        console.log("Data received..");
+      });
+  }, []);
 
   return (
-    <div>   
-      <Header as='h2'content='Reactivities' icon='users'/>
-        <List>
-          {activities.map((activity) => (
-            <List.Item key={activity.id}>
-              {activity.title}, {activity.description}
-            </List.Item>
-          ))}
-        </List>
+    <div>
+      <Header as="h2" content="Reactivities" icon="users" />
+      <List>
+        <ActivityDashboard activities={activities} />
+      </List>
     </div>
   );
 }
